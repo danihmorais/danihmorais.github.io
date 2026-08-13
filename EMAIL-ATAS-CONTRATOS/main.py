1f8746877b5bbc6762d7c58359363b7b83204563
danihmorais <67606539+danihmorais@users.noreply.github.com>
2026-08-13 12:20:25 -0300

    Correções iniciais

--- EMAIL-ATAS-CONTRATOS/main.py
+++ EMAIL-ATAS-CONTRATOS/main.py
@@ -20,8 +20,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://danihmorais.github.io",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
