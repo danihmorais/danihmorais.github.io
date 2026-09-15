35d6d3fa184ecdaea08a0194a74ce3375467352b
danihmorais <67606539+danihmorais@users.noreply.github.com>
2026-09-15 20:40:25 -0300

    Remover servidor independente da Biblioteca

--- BIBLIOTECA-MUNICIPAL/backend/server.py
+++
@@ -1,9 +0,0 @@
-import main
-main.init_db()
-import bootstrap_admin
-import access_routes
-import route_fix
-import security
-import uvicorn
-
-uvicorn.run(route_fix.main.app, host="127.0.0.1", port=9100)
